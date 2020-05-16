8637ab982077f235e851828a9912c05c73a40ad4
Biki Tirua <35054593+BickyTirua@users.noreply.github.com>
2020-05-16 14:30:37 +0530

    Add files via upload

---
+++ portfolio_CSS_HTM_Bootsrao_particles.js_typ.JS/js/particles.js
@@ -0,0 +1,118 @@
+
+particlesJS('particles-js',
+
+    {
+        "particles": {
+            "number": {
+                "value": 6,
+                "density": {
+                    "enable": true,
+                    "value_area": 600
+                }
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "shape": {
+                "type": "image",
+                "stroke": {
+                    "width": 3,
+                    "color": "grey"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src":"./assets/bnnr.png",
+                    // "src": "https://media.giphy.com/media/AOSwwqVjNZlDO/giphy.gif",
+
+                    "width": 100,
+                    "height": 100
+
+                }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 30,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 100,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 3
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": true,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": false,
+                    "mode": "repulse"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 800,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 800,
+                    "size": 80,
+                    "duration": 2,
+                    "opacity": 0.8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 400,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    }
+
+);
